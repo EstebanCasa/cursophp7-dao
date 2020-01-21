0fe2480aa63a13631f65ddd56d74ddb8f0683806
Fazt <contact@faztweb.com>
2020-01-21 20:49:27 -0300

    criamos a classe Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -6,20 +6,24 @@ class Sql extends PDO {
 
     public function __construct(){
 
-        $this->conn = new PDO("mysql:host=localhost;dbname=php7", "root","root");
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root","root");
     }
     
     private function setParams($statement, $parameters = array()){
+        
         foreach($parameters as $key => $value){
-            $this->setParam($statement,$key, $value);
+            
+            $this->setParam($statement, $key, $value);
         }
     }
 
     private function setParam($statement,$key,$value){
+
         $statement->bindParam($key,$value);
     }
 
     public function query($rawQuery, $params = array()){
+
         $stmt = $this->conn->prepare($rawQuery);
 
         $this->setParams($stmt,$params);
@@ -34,9 +38,6 @@ class Sql extends PDO {
         $stmt = $this->query($rawQuery, $params);
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 
     }
     
